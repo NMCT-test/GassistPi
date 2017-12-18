5d7937e14d7109e72cb5b2a141aae5ac2ad99f04
nmctseb <29257008+nmctseb@users.noreply.github.com>
2017-12-18 18:19:40 +0100

    reformat

--- src/actions.py
+++ src/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-#This is different from AIY Kit's actions
-#Copying and Pasting AIY Kit's actions commands will not work
+# This is different from AIY Kit's actions
+# Copying and Pasting AIY Kit's actions commands will not work
 
 import os
 import os.path
@@ -14,49 +14,52 @@ import feedparser
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-#Number of entities in 'var' and 'PINS' should be the same
-var = ('kitchen lights', 'bathroom lights', 'bedroom lights')#Add whatever names you want. This is case is insensitive
-gpio = (12,13,24)#GPIOS for 'var'. Add other GPIOs that you want
+# Number of entities in 'var' and 'PINS' should be the same
+var = (
+'kitchen lights', 'bathroom lights', 'bedroom lights')  # Add whatever names you want. This is case is insensitive
+gpio = (12, 13, 24)  # GPIOS for 'var'. Add other GPIOs that you want
 
-#Number of station names and station links should be the same
-stnname=('Radio One', 'Radio 2', 'Radio 3', 'Radio 5')#Add more stations if you want
-stnlink=('http://www.radiofeeds.co.uk/bbcradio2.pls', 'http://www.radiofeeds.co.uk/bbc6music.pls', 'http://c5icy.prod.playlists.ihrhls.com/1469_icy', 'http://playerservices.streamtheworld.com/api/livestream-redirect/ARNCITY.mp3')
+# Number of station names and station links should be the same
+stnname = ('Radio One', 'Radio 2', 'Radio 3', 'Radio 5')  # Add more stations if you want
+stnlink = ('http://www.radiofeeds.co.uk/bbcradio2.pls', 'http://www.radiofeeds.co.uk/bbc6music.pls',
+           'http://c5icy.prod.playlists.ihrhls.com/1469_icy',
+           'http://playerservices.streamtheworld.com/api/livestream-redirect/ARNCITY.mp3')
 
-#IP Address of ESP
-ip='xxxxxxxxxxxx'
+# IP Address of ESP
+ip = 'xxxxxxxxxxxx'
 
-#Declaration of ESP names
-devname=('Device 1', 'Device 2', 'Device 3')
-devid=('/Device1', '/Device2', '/Device3')
+# Declaration of ESP names
+devname = ('Device 1', 'Device 2', 'Device 3')
+devid = ('/Device1', '/Device2', '/Device3')
 
 for pin in gpio:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
 
-#Servo pin declaration
+# Servo pin declaration
 GPIO.setup(27, GPIO.OUT)
-pwm=GPIO.PWM(27, 50)
+pwm = GPIO.PWM(27, 50)
 pwm.start(0)
 
-#Stopbutton
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+# Stopbutton
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#Led Indicator
+# Led Indicator
 GPIO.setup(25, GPIO.OUT)
-led=GPIO.PWM(25,1)
+led = GPIO.PWM(25, 1)
 led.start(0)
 
 playshell = None
 
-#Parcel Tracking declarations
-#If you want to use parcel tracking, register for a free account at: https://www.aftership.com
-#Add the API number and uncomment next two lines
-#api = aftership.APIv4('YOUR-AFTERSHIP-API-NUMBER')
-#couriers = api.couriers.all.get()
+# Parcel Tracking declarations
+# If you want to use parcel tracking, register for a free account at: https://www.aftership.com
+# Add the API number and uncomment next two lines
+# api = aftership.APIv4('YOUR-AFTERSHIP-API-NUMBER')
+# couriers = api.couriers.all.get()
 number = ''
-slug=''
+slug = ''
 
-#RSS feed URLS
+# RSS feed URLS
 worldnews = "http://feeds.bbci.co.uk/news/world/rss.xml"
 technews = "http://feeds.bbci.co.uk/news/technology/rss.xml"
 topnews = "http://feeds.bbci.co.uk/news/rss.xml"
@@ -64,41 +67,44 @@ sportsnews = "http://feeds.feedburner.com/ndtvsports-latest"
 quote = "http://feeds.feedburner.com/brainyquote/QUOTEBR"
 
 
-#Text to speech converter
+# Text to speech converter
 def say(words):
     tempfile = "temp.wav"
-    devnull = open("/dev/null","w")
-    lang = "en-GB" #Other languages: en-US: US English, en-GB: UK English, de-DE: German, es-ES: Spanish, fr-FR: French, it-IT: Italian
-    subprocess.call(["pico2wave", "-w", tempfile, "-l", lang,  words],stderr=devnull)
-    subprocess.call(["aplay", tempfile],stderr=devnull)
+    devnull = open("/dev/null", "w")
+    lang = "en-GB"  # Other languages: en-US: US English, en-GB: UK English, de-DE: German, es-ES: Spanish, fr-FR: French, it-IT: Italian
+    subprocess.call(["pico2wave", "-w", tempfile, "-l", lang, words], stderr=devnull)
+    subprocess.call(["aplay", tempfile], stderr=devnull)
     os.remove(tempfile)
 
-#Radio Station Streaming
+
+# Radio Station Streaming
 def radio(phrase):
     for num, name in enumerate(stnname):
         if name.lower() in phrase:
-            station=stnlink[num]
+            station = stnlink[num]
             say("Tuning into " + name)
-            p = subprocess.Popen(["/usr/bin/vlc",station],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+            p = subprocess.Popen(["/usr/bin/vlc", station], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-#ESP6266 Devcies control
+
+# ESP6266 Devcies control
 def ESP(phrase):
     for num, name in enumerate(devname):
         if name.lower() in phrase:
-            dev=devid[num]
+            dev = devid[num]
             if 'on' in phrase:
-                ctrl='=ON'
+                ctrl = '=ON'
                 say("Turning On " + name)
             elif 'off' in phrase:
-                ctrl='=OFF'
+                ctrl = '=OFF'
                 say("Turning Off " + name)
-            subprocess.Popen(["elinks", ip + dev + ctrl],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+            subprocess.Popen(["elinks", ip + dev + ctrl], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             time.sleep(2)
-            subprocess.Popen(["/usr/bin/pkill","elinks"],stdin=subprocess.PIPE)
+            subprocess.Popen(["/usr/bin/pkill", "elinks"], stdin=subprocess.PIPE)
 
-#Stepper Motor control
+
+# Stepper Motor control
 def SetAngle(angle):
-    duty = angle/18 + 2
+    duty = angle / 18 + 2
     GPIO.output(27, True)
     say("Moving motor by " + str(angle) + " degrees")
     pwm.ChangeDutyCycle(duty)
@@ -106,92 +112,95 @@ def SetAngle(angle):
     pwm.ChangeDutyCycle(0)
     GPIO.output(27, False)
 
-#Play Youtube Music
+
+# Play Youtube Music
 def YouTube(phrase):
-    idx=phrase.find('play')
-    track=phrase[idx:]
-    track=track.replace("'}", "",1)
-    track = track.replace('play','',1)
-    track=track.strip()
+    idx = phrase.find('play')
+    track = phrase[idx:]
+    track = track.replace("'}", "", 1)
+    track = track.replace('play', '', 1)
+    track = track.strip()
     global playshell
     if (playshell == None):
-        playshell = subprocess.Popen(["/usr/local/bin/mpsyt",""],stdin=subprocess.PIPE ,stdout=subprocess.PIPE)
+        playshell = subprocess.Popen(["/usr/local/bin/mpsyt", ""], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     print("Playing: " + track)
     say("Playing " + track)
-    playshell.stdin.write(bytes('/' + track + '\n1\n','utf-8'))
+    playshell.stdin.write(bytes('/' + track + '\n1\n', 'utf-8'))
     playshell.stdin.flush()
 
+
 def stop():
-    pkill = subprocess.Popen(["/usr/bin/pkill","vlc"],stdin=subprocess.PIPE)
+    pkill = subprocess.Popen(["/usr/bin/pkill", "vlc"], stdin=subprocess.PIPE)
 
-#Parcel Tracking
+
+# Parcel Tracking
 def track():
-    text=api.trackings.get(tracking=dict(slug=slug, tracking_number=number))
-    numtrack=len(text['trackings'])
+    text = api.trackings.get(tracking=dict(slug=slug, tracking_number=number))
+    numtrack = len(text['trackings'])
     print("Total Number of Parcels: " + str(numtrack))
-    if numtrack==0:
-        parcelnotify=("You have no parcel to track")
+    if numtrack == 0:
+        parcelnotify = ("You have no parcel to track")
         say(parcelnotify)
-    elif numtrack==1:
-        parcelnotify=("You have one parcel to track")
+    elif numtrack == 1:
+        parcelnotify = ("You have one parcel to track")
         say(parcelnotify)
-    elif numtrack>1:
-        parcelnotify=( "You have " + str(numtrack) + " parcels to track")
+    elif numtrack > 1:
+        parcelnotify = ("You have " + str(numtrack) + " parcels to track")
         say(parcelnotify)
-    for x in range(0,numtrack):
-        numcheck=len(text[ 'trackings'][x]['checkpoints'])
-        description = text['trackings'][x]['checkpoints'][numcheck-1]['message']
-        parcelid=text['trackings'][x]['tracking_number']
-        trackinfo= ("Parcel Number " + str(x+1)+ " with tracking id " + parcelid + " is "+ description)
+    for x in range(0, numtrack):
+        numcheck = len(text['trackings'][x]['checkpoints'])
+        description = text['trackings'][x]['checkpoints'][numcheck - 1]['message']
+        parcelid = text['trackings'][x]['tracking_number']
+        trackinfo = ("Parcel Number " + str(x + 1) + " with tracking id " + parcelid + " is " + description)
         say(trackinfo)
-        #time.sleep(10)
+        # time.sleep(10)
 
-#RSS Feed Reader
+
+# RSS Feed Reader
 def feed(phrase):
     if 'world news' in phrase:
-        URL=worldnews
+        URL = worldnews
     elif 'top news' in phrase:
-        URL=topnews
+        URL = topnews
     elif 'sports news' in phrase:
-        URL=sportsnews
+        URL = sportsnews
     elif 'tech news' in phrase:
-        URL=technews
+        URL = technews
     elif 'my feed' in phrase:
-        URL=quote
-    numfeeds=10
-    feed=feedparser.parse(URL)
-    feedlength=len(feed['entries'])
+        URL = quote
+    numfeeds = 10
+    feed = feedparser.parse(URL)
+    feedlength = len(feed['entries'])
     print(feedlength)
-    if feedlength<numfeeds:
-        numfeeds=feedlength
-    title=feed['feed']['title']
+    if feedlength < numfeeds:
+        numfeeds = feedlength
+    title = feed['feed']['title']
     say(title)
-    #To stop the feed, press and hold stop button
+    # To stop the feed, press and hold stop button
     while GPIO.input(23):
-        for x in range(0,numfeeds):
-            content=feed['entries'][x]['title']
+        for x in range(0, numfeeds):
+            content = feed['entries'][x]['title']
             print(content)
             say(content)
-            summary=feed['entries'][x]['summary']
+            summary = feed['entries'][x]['summary']
             print(summary)
             say(summary)
             if not GPIO.input(23):
-              break
-        if x == numfeeds-1:
+                break
+        if x == numfeeds - 1:
             break
         else:
             continue
 
-    
 
-#GPIO Device Control
+# GPIO Device Control
 def Action(phrase):
     if 'shut down' in phrase:
         say('Shutting down Raspberry Pi')
         time.sleep(10)
         os.system("sudo shutdown -h now")
-        #subprocess.call(["shutdown -h now"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # subprocess.call(["shutdown -h now"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if 'motor' in phrase:
         for s in re.findall(r'\b\d+\b', phrase):
             SetAngle(int(s))
@@ -200,7 +209,7 @@ def Action(phrase):
     else:
         for num, name in enumerate(var):
             if name.lower() in phrase:
-                pinout=gpio[num]
+                pinout = gpio[num]
                 if 'on' in phrase:
                     GPIO.output(pinout, 1)
                     say("Turning On " + name)
--- src/snowboy.py
+++ src/snowboy.py
@@ -1,32 +1,35 @@
-import snowboydecoder
+from .snowboydecoder import HotwordDetector, play_audio_file, DETECT_DING, DETECT_DONG
 import sys
 import signal
 import RPi.GPIO as GPIO
 import time
 import os
 import subprocess
-from assistant import Assistant
-subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/customwakeword.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+from .assistant import Assistant
+
+subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/customwakeword.wav"], stdin=subprocess.PIPE,
+                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Demo code for listening two hotwords at the same time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 interrupted = False
-GPIO.setup(22,GPIO.OUT)
-GPIO.output(22,GPIO.LOW)
+GPIO.setup(22, GPIO.OUT)
+GPIO.output(22, GPIO.LOW)
 
-#Add your custom models here
+# Add your custom models here
 models = ['/home/pi/GassistPi/src/resources/alexa.umdl', '/home/pi/GassistPi/src/resources/snowboy.umdl']
+
 
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
 
-
 def interrupt_callback():
     global interrupted
     return interrupted
+
 
 ##if len(sys.argv) != 3:
 ##    print("Error: need to specify 2 model names")
@@ -34,24 +37,24 @@ def interrupt_callback():
 ##    sys.exit(-1)
 gassist = Assistant()
 
+
 def detected():
     detector.terminate()
-    GPIO.output(22,GPIO.HIGH)
+    GPIO.output(22, GPIO.HIGH)
     time.sleep(.05)
-    GPIO.output(22,GPIO.LOW)
-    snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
+    GPIO.output(22, GPIO.LOW)
+    play_audio_file(DETECT_DING)
     gassist.assist()
     detector.start(detected_callback=callbacks,
                    interrupt_check=interrupt_callback,
                    sleep_time=0.03)
 
 
-
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-sensitivity = [0.5]*len(models)
-detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
+sensitivity = [0.5] * len(models)
+detector = HotwordDetector(models, sensitivity=sensitivity)
 callbacks = [detected, detected]
 print('Listening... Press Ctrl+C to exit')
 
